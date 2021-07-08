746b0652d9d6051941a785d892a9d4d3a617a5e8
Nick Ritchie <nick.alex.ritchie@gmail.com>
2021-07-08 11:23:18 +1000

    massively simplified color change

--- assets/js/utility.js
+++ assets/js/utility.js
@@ -1,44 +1,33 @@
-let primaries =    ['#FF7F60','#8DA4CF','#F22738','#2E4159','#D93662','#385663','#87878C','#4850E8','#0388A6','#0C2CFA','#F2F2F2'];
-let secondaries =  ['#FFB26D','#9EA9CE','#A63247','#51718C','#3A3873','#F8CACC','#01070D','#724BF2','#63D8F2','#0B5FDE','#F2B705'];
-let tertiaries =   ['#FDD272','#B7B3CC','#3E3740','#698FB6','#04BFAD','#F2F2F2','#0A1926','#954FDB','#F29F05','#00A3F5','#0477BF'];
-let quaternaries = ['#3EB2A2','#E2C3CC','#F2DDD0','#91B7D9','#F2AF5C','#E4E4E4','#384D59','#CB4BF2','#F28705','#0BD0DE','#BF04A0'];
-let quinaries =    ['#385663','#F8CACC','#F2F2F2','#E4E4E4','#F2695C','#F2695C','#A9C6D9','#E848DB','#F27405','#0CFACA','#F2059F'];
-
-// random color generator
-function getRandomColorIndex(){
-    let max = primaries.length;
-    return Math.floor(Math.random()*max+1);
-}
+// global pallet combinations, primary-secondary-tertiary etc limited to 5
+pallettes = [
+    ['#FF7F60', '#FFB26D', '#FDD272', '#3EB2A2', '#385663'],
+    ['#8DA4CF', '#9EA9CE', '#B7B3CC', '#E2C3CC', '#F8CACC'],
+    ['#F22738', '#A63247','#3E3740','#F2DDD0','#F2F2F2'],
+    ['#2E4159', '#51718C','#698FB6','#91B7D9','#E4E4E4'],
+]
 
 // change colors in :root dom element
 function ChangeColor(){
     // get the style variables in root
     let r = document.querySelector(':root');
-    // get the computed style of that element
-    let rs = getComputedStyle(r);
-    // get the index in the primaries array from the current style value
-    let currentIndex = getCurrentPrimaryScheme(rs.getPropertyValue('--primary'));
-    // increment
-    let newIndex = currentIndex + 1;
-
-    // make sure no overload
-    if (newIndex === primaries.length){
-        newIndex=0;
-    }
-
+    let pallette = randomPallette();
     // set all the properties to the new index
-    r.style.setProperty('--primary', primaries[newIndex]);
-    r.style.setProperty('--secondary', secondaries[newIndex]);
-    r.style.setProperty('--tertiary', tertiaries[newIndex]);
-    r.style.setProperty('--quaternary', quaternaries[newIndex]);
-    r.style.setProperty('--quinary', quinaries[newIndex]);
-
-    console.log(r.style);
+    r.style.setProperty('--primary', pallette[0]);
+    r.style.setProperty('--secondary', pallette[1]);
+    r.style.setProperty('--tertiary', pallette[2]);
+    r.style.setProperty('--quaternary', pallette[3]);
+    r.style.setProperty('--quinary', pallette[4]);
 }
 
-// get current primary value index from color schemes - figure out where the current scheme is in the arrays
-function getCurrentPrimaryScheme(colorCode) {
-    return primaries.findIndex(x => x === colorCode);
+// generate a random integer inside a range
+function getRandomIntFromRange(min, max){
+    return Math.floor(Math.random()*(max-min+1)+min);
+};
+
+// get a random color pallette
+function randomPallette(){
+    let randomPallette = pallettes[getRandomIntFromRange(0, 4)];
+    return randomPallette;
 }
 
 // get the change color button as element
