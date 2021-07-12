87cba7d9cd6c13321c01e0bbbd76ad30c0ec3acf
Nick <nick.alex.ritchie@gmail.com>
2021-07-12 20:58:37 +1000

    built save, load and reset functions

---
+++ assets/js/nick.js
@@ -0,0 +1,90 @@
+// functions to save and load movie objects
+
+/*
+movie_details_obj = {
+  id: 'tt1234567',
+  title: 'v for vendetta',
+  released: moment().format('y'),
+  ratingsCount : 300,
+  rating: 4.5,
+  imageUrl: "path_to_url",
+  Genres: ['x', 'y', 'z'],
+  plotOutline: "a long string describing the movie",
+}
+
+searchObject = {
+  actor_id: "nm1231234",
+  actor_name: "natalie portman",
+  actor_img_url: "path to url for actor",
+  movie_objs = [movieObject1, movieObject2,] 
+}
+*/
+
+storageName = 'searchListObjects'
+testMemoryFunctions();
+
+function testMemoryFunctions(){
+    let testSearchObject = 'This is a test';
+    let test2SearchObject = 'This is a second test string';
+
+    // reset test
+    resetMemory();
+    console.log('\tShould have printed out: Memory reset')
+    
+    // empty state test
+    saveSearchObject(testSearchObject);
+    console.log('\tShould have printed out: Created new local storage list and appended result');
+
+    // adding to state test
+    saveSearchObject(test2SearchObject);
+    console.log('\tShould have printed out: Found search objects in storage, appended element onto list');
+
+    // checking load test
+    let recoveredResult = loadSearchObjects();
+    console.log('\tShould have printed out: Found results in local storage');
+
+    // final reset and load test
+    resetMemory();
+    console.log('\tShould have printed out: Memory reset');
+    // should return nothing
+    let emptySet = loadSearchObjects();
+    console.log('\tShould have printed out: No results in local storage at the moment, returning null')
+}
+
+// accepts a searchObject and appends it to locally stored results, if empty, creates a storage list
+function saveSearchObject(newSearchObject){
+    // load the current list in storage of searchObjects
+    let storage = JSON.parse(localStorage.getItem(storageName));
+    // if it finds there are results already, add the result to the end
+    if(storage){
+        console.log('Found search objects in storage, appended element onto list');
+        storage.push(newSearchObject);
+    } else {
+        // creates an empty string and puts the searchObject into the list for saving
+        console.log('Created new local storage list and appended result')
+        storage = [newSearchObject, ];
+    }
+    // turn storage element into a string for storage
+    let stringStorage = JSON.stringify(storage);
+    // save to local storage
+    localStorage.setItem(storageName, stringStorage);
+}
+
+// function that loads all search Objects from local storage and returns them in a list
+// - if local storage is empty returns special null - not string
+function loadSearchObjects(){
+    let PastStorage = JSON.parse(localStorage.getItem(storageName));
+    if (PastStorage){
+        console.log('Found results in local storage')
+        return PastStorage;
+    } else {
+        console.log('No results in local storage at the moment, returning null')
+        return null;
+    }
+}
+
+// used to completely remove local storage elements
+function resetMemory(event){
+    localStorage.clear();
+    console.log('Memory reset')
+}
