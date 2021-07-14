cc6c885d892158c639c68bc7f98fb44f3bf883c6
Nick <nick.alex.ritchie@gmail.com>
2021-07-14 14:00:18 +1000

    major cleanup

---
+++ assets/js/classes.js
@@ -0,0 +1,165 @@
+// name used by save and load functions to refer to the file object
+storageName = 'searchListObjects'
+
+// function that gets the most recent search
+function getMostRecentSearchObject(){
+    let searchObjects = loadSearchObjects();
+    if(searchObjects){
+        return searchObjects[0];
+    }else{
+        return null
+    }
+}
+
+// function that loads all search Objects from local storage and returns them in a list
+// - if local storage is empty returns special null - not string
+function loadSearchObjects(){
+    let PastStorage = JSON.parse(localStorage.getItem(storageName));
+    if (PastStorage){
+        console.log('Found results in local storage')
+        return PastStorage;
+    } else {
+        console.log('No results in local storage at the moment, returning null')
+        return null;
+    }
+}
+
+// used to completely remove local storage elements
+function resetMemory(){
+    localStorage.clear();
+    console.log('Memory reset')
+}
+
+// accepts a searchObject and appends it to locally stored results, if empty, creates a storage list
+function saveSearchObject(newSearchObject){
+    // load the current list in storage of searchObjects
+    let storage = JSON.parse(localStorage.getItem(storageName));
+    // if it finds there are results already, add the result to the end
+    if(storage){
+        console.log('Found search objects in storage, appended element onto list');
+        storage.push(newSearchObject);
+    } else {
+        // creates an empty string and puts the searchObject into the list for saving
+        console.log('Created new local storage list and appended result')
+        storage = [newSearchObject, ];
+    }
+    // turn storage element into a string for storage
+    let stringStorage = JSON.stringify(storage);
+    // save to local storage
+    localStorage.setItem(storageName, stringStorage);
+}
+
+function testMemoryFunctions(){
+    let testSearchObject = 'This is a test';
+    let test2SearchObject = 'This is a second test string';
+
+    // reset test
+    resetMemory();
+    console.log('\tShould have printed out: Memory reset')
+    
+    // empty state test
+    saveSearchObject(testSearchObject);
+    console.log('\tShould have printed out: Created new local storage list and appended result');
+
+    // adding to state test
+    saveSearchObject(test2SearchObject);
+    console.log('\tShould have printed out: Found search objects in storage, appended element onto list');
+
+    // checking load test
+    let recoveredResult = loadSearchObjects();
+    console.log('\tShould have printed out: Found results in local storage');
+
+    // final reset and load test
+    resetMemory();
+    console.log('\tShould have printed out: Memory reset');
+    // should return nothing
+    let emptySet = loadSearchObjects();
+    console.log('\tShould have printed out: No results in local storage at the moment, returning null')
+}
+
+function testObjectBuild(){
+    let movieObject1 = new movieObject('tt1234567', 
+        'V for vendetta', 
+        2009, 
+        1300, 
+        3.5, 
+        'https://m.media-amazon.com/images/M/MV5BOTI5ODc3NzExNV5BMl5BanBnXkFtZTcwNzYxNzQzMw@@._V1_.jpg',
+        ['drama', 'action'],
+        'A guy blows up a building with a mask on')
+
+    let movieObject2 = new movieObject('tt2345678', 
+        'star wars episode 1', 
+        2004, 
+        3000, 
+        3.1, 
+        'https://static.wikia.nocookie.net/starwars/images/b/ba/Ep1_PC_front.jpg/revision/latest?cb=20170521222822',
+        ['sci-fi', 'action'],
+        'The jedi do stuff')
+    
+
+    let search = new actorObject('nm1234567', 
+        'natalie portman',
+        'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ-umCzL4zUJ6W1OCXyoYkbwHhkKLS9ks8YP3rh6y1W_iQtPOnh',
+        [movieObject1, movieObject2]
+        )
+
+    console.log(search);
+}
+
+// class that builds searchObjects
+// class accepts
+/*
+actorObj1           - actorObject
+actorObj2           - actorObject
+shared_movie_list   - list of movieObjects - after filter
+*/
+class searchObject{
+    constructor(actor1, actor2, movieList){
+        this.actor1 = actor1;
+        this.actor2 = actor2;
+        this.movieList = movieList;
+    }
+}
+
+// class accepts
+/*
+actor_id            -str eg 'nm1234567'
+actor_name          -str eg 'natalie portman'
+actor_img           -str eg 'http://path_to_thing.png'
+movie_object_list   -list of MovieObjects eg [MovieObject1, MovieObject2,]
+*/
+// constructor that builds Actor Objects
+class actorObject {
+    constructor(actor_id, actor_name, actor_img, movie_object_list){
+        this.actor_id = actor_id;
+        this.actor_name = actor_name;
+        this.actor_img = actor_img;
+        this.movie_object_list = movie_object_list;
+    }
+}
+
+// constructor that build Movie sub objects
+// class accepts 
+/* 
+id              -number eg 1
+title           -string eg 'v for vendetta'
+released        -moment().format('y) object 
+ratingsCount    -number
+rating          -number
+imageUrl        -string
+genres          -list of strings eg ['action', 'comedy', 'drama']
+plotOutline     -string
+*/
+class movieObject {
+    constructor(id, title, released, ratingsCount, rating, imageUrl, genres, plotOutline){
+        this.id = id;
+        this.title = title;
+        this.released = released;
+        this.ratingsCount = ratingsCount;
+        this.rating = rating;
+        this.imageUrl = imageUrl;
+        this.genres = genres;
+        this.plotOutline = plotOutline;
+    }
+}
+
--- assets/js/runtime.js
+++ assets/js/runtime.js
@@ -1,8 +1,37 @@
 // add event listener for the form
 let searchButton = $('#search_button');
-searchButton.on('click', validateFormDetails);
-actorList = [];
+searchButton.on('click', validateFormAndSearch);
 
+// run search
+// validating the form details
+function validateFormAndSearch(event){
+  event.preventDefault();
+
+  let buttonElement = $(event.target)
+  let parentForm = buttonElement.parent();
+
+  // get the two input fields from the form
+  let userInputs = parentForm.children().find('input');
+
+  // get form elements
+  let userInputEl1 = userInputs[0];
+  let userInputEl2 = userInputs[1];
+  
+  let UserInputText1 = userInputEl1.value;
+  let UserInputText2 = userInputEl2.value;
+  
+  console.log(UserInputText1);
+  console.log(UserInputText2);
+  // this method should scale to any number of query's
+  let inputStringArray = [
+      buildQueryStringForIMDb(UserInputText1),
+      buildQueryStringForIMDb(UserInputText2),
+  ]
+  // getActorIdsFromInputs(inputStringArray);
+  runSearchWithInputValues(inputStringArray);
+}
+
+// details for the api queries
 apiDetails = {
     "method": "GET",
     "headers": {
@@ -11,37 +40,22 @@ apiDetails = {
     }
 }
 
-// validating the form details
-function validateFormDetails(event){
-    event.preventDefault();
-
-    let buttonElement = $(event.target)
-    let parentForm = buttonElement.parent();
-
-    // get the two input fields from the form
-    let userInputs = parentForm.children().find('input');
-
-    // get form elements
-    let userInputEl1 = userInputs[0];
-    let userInputEl2 = userInputs[1];
-    
-    let UserInputText1 = userInputEl1.value;
-    let UserInputText2 = userInputEl2.value;
-    
-    console.log(UserInputText1);
-    console.log(UserInputText2);
-    // this method should scale to any number of query's
-    let inputStringArray = [
-        buildQueryStringForIMDb(UserInputText1),
-        buildQueryStringForIMDb(UserInputText2),
-    ]
-    // getActorIdsFromInputs(inputStringArray);
-    fetchMoviesFromInput(inputStringArray);
+// Chris's matching function
+function getCommonMovieObjects(actorObj1, actorObj2){
+  let resultMovieList = [];
+  for(let j=0; j< actorObj2.movie_object_list.length; j++){
+    for(let i = 0; i<actorObj1.movie_object_list.length; i++){
+      if(actorObj2.movie_object_list[j].id === actorObj1.movie_object_list[i].id){
+        resultMovieList.push(actorObj1.movie_object_list[i]);
+      }
+    }
+  }
+  return resultMovieList;
 }
 
+// function makes the query string from user input to query the auto-complete endpoint
 function buildQueryStringForIMDb(userInput){
     // using IMDb API
-    // https://rapidapi.com/apidojo/api/imdb8/
     let rootFilmographyApi = "https://imdb8.p.rapidapi.com/auto-complete?q="
 
     // this trims leading and trailing spaces, and replaces middle spaces with
@@ -52,81 +66,34 @@ function buildQueryStringForIMDb(userInput){
     return queryString;
 }
 
-function getActorIdsFromInputs(queryStrings){
-    console.log(queryStrings);
-    queryStrings.forEach((element) => {
-
-        fetch(element, apiDetails)
-        .then(response => {
-            return response.json();
-        })
-        .then(data =>{
-            // get the actor details from the first element of the returned data - most popular video they did I think
-            let ActorId = data.d[0].id;
-            let ActorName = data.d[0].l;
-            let ActorImage = data.d[0].i.imageUrl;
-            // await this function call as it is an asynchronous fetch
-            let actorMovieList = getFilmographyFromActorId(data.d[0].id);
-            // then make new object
-            console.log('Actor: ',ActorName,'returned: ',actorMovieList.length,' movies from the filmography function');
-            let actor = new actorObject(ActorId, ActorName, ActorImage, actorMovieList);
-            actorList.push(actor);
-        })
-        .catch(err => {
-            console.error(err);
-        });
-    });
-}
-
-async function getFilmographyFromActorId(actorId){ //rip the title ids from the filmography to the actor objects
-    let filmographyApiUrlRoot = "https://imdb8.p.rapidapi.com/actors/get-all-filmography?nconst="
-    let actorMovieList = [];
-    try{
-        let response = await fetch(filmographyApiUrlRoot + actorId, apiDetails);
-        let json = await response.json();
-        
-        
-        console.log('found: ',json.filmography.length,' movies in the api call');
-        for(let i=0; i < json.filmography.length; i++){
-            if(json.filmography[i].titleType == "movie", 
-            json.filmography[i].category == "actor"){
-                let MovieId = json.filmography[i].id;
-                MovieId = MovieId.substring(7, 16);
-                actorMovieList.push(MovieId);
-            }
-        }
-        return actorMovieList;
-        
-    }catch{(error => {
-        console.log(error);
-    })}
-}
-
+// fetches the movie list response from the filmography endpoint
 async function fetchMovieListFromActor(actorObj){
     let filmographyApiUrlRoot = "https://imdb8.p.rapidapi.com/actors/get-all-filmography?nconst=";
     let response = await fetch(filmographyApiUrlRoot + actorObj.actor_id, apiDetails);
     return response;
 }
 
+// processes the movie list json object into a js object
 function processMovieListJson(jsonObject){
     let actorMovieList = [];
 
     for(let i=0; i < jsonObject.filmography.length; i++){
 
-        let movie = jsonObject.filmography[i];
+        let movieObj = jsonObject.filmography[i];
 
-        let titleType = movie.titleType;
-        let category = movie.category;
-        let movieId = movie.id.substring(7, 16);
+        let titleType = movieObj.titleType;
+        let category = movieObj.category;
+        // let movieId = movieObj.id.substring(7, 16);
         // note that hugo weaving was an actor and Natalie Portman was an actress
         if(titleType === "movie" && (category === "actor" || category === "actress")){
-            actorMovieList.push(movieId);
+            actorMovieList.push(movieObj);
         }
     }
     return actorMovieList;
 }
 
-async function fetchMoviesFromInput(searchStrings){
+// runs the search for the search strings from page input
+async function runSearchWithInputValues(searchStrings){
     let input1 = searchStrings[0];
     let input2 = searchStrings[1];
     let debug = true;
@@ -199,35 +166,11 @@ async function fetchMoviesFromInput(searchStrings){
     actor2obj.movie_object_list = movieList2;
 
     // this is where we use chris's matching function to get the shared movie list
-
-    //placeholder
-    chrisMatchedMovieList = ['', '', ''];
+    // make the list of objects that match and save it to the search object
+    let matchedMovies = getCommonMovieObjects(actor1obj, actor2obj);
 
     // create a new search object with both actor objects and the matched movie list
-    search_object = new SearchObject(actor1obj, actor2obj, chrisMatchedMovieList)
+    let search_object = new searchObject(actor1obj, actor2obj, matchedMovies);
 
     saveSearchObject(search_object);
 }
-
-function obtainMovieData(ID){
-    let overviewApiUrlRoot = "https://imdb8.p.rapidapi.com/title/get-overview-details?tconst=";
-    let urlDetail = "7&currentCountry=US";
-    fetch(overviewApiUrlRoot + ID + urlDetail, apiDetails)
-.then(response => {
-	return response.json();
-})
-.then(data =>{
-    let MvTitle = data.title.title;
-    let MvReleased = data.title.year;
-    let MvRatingscount = data.ratings.ratingsCount;
-    let MvRating = data.ratings.rating;
-    let MvImageUrl = data.title.image.url;
-    let MvGenres = data.genres;
-    let MvPlotOutline = data.plotOutline.text;
-})
-.catch(err => {
-	console.error(err);
-});
-}
-    
-
